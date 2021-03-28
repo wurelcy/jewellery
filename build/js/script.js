d28bfdea106368ac6246cab2330c470a054ab5ee
wurelcy <olya.sebro@gmail.com>
2021-03-28 23:23:58 +0300

    Добавляет страницу каталога и реализует мобильное меню

--- build/js/script.js
+++ build/js/script.js
@@ -1,30 +1,49 @@
 'use strict';
 
 $(document).ready(function () {
-  $('.slider__list').slick({
-    dots: true,
-    infinite: true,
-    speed: 600,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
+  let slider = document.querySelector('.slider__list');
+
+  if (slider) {
+  $(slider).slick({
+      dots: true,
+      infinite: true,
+      speed: 600,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
         }
-      }
-    ]
-  });
+      ]
+    });
+  }
 
   let arrowPrev = document.querySelector('.slick-prev');
   let arrowNext = document.querySelector('.slick-next');
   let dotsList = document.querySelector('.slick-dots');
 
+  let header = document.querySelector('.header');
+  let headerToggle = document.querySelector('.header__toggle');
+
   const accordionBlocks = document.querySelectorAll('.faq__item');
+
+  if (header) {
+    headerToggle.addEventListener('click', () => {
+      if (header.classList.contains('header--closed')) {
+        header.classList.add('header--opened');
+        header.classList.remove('header--closed');
+      } else {
+        header.classList.add('header--closed');
+        header.classList.remove('header--opened');
+      }
+    });
+  }
 
   if (dotsList) {
     dotsList.style.display = 'flex';
